e09c9bfee6c5eba60296801e5c0e4c1d3b0fca67
unknown <instructor@RM56-PC01.commoncore.com>
2019-04-23 11:39:59 -0700

    Generates file on users desktop

--- src/script.py
+++ src/script.py
@@ -1,4 +1,18 @@
 # This program creates a text document that can be uploaded to moodle server.
 # By Wontez D Morgan wontez@protonmail.com 23 April 2019
 
+from pathlib import Path 
 
+# Creat code to get user input and display user interface
+
+
+# Gets home directory and sets working directory to the users Desktop
+path = Path.home()
+desktop = path / 'Desktop'
+
+# Creates file on users Desktop
+uploadFile = desktop / 'newfile.txt'
+Path(uploadFile).touch()
+
+# Writes content to the file created on the users Desktop
+Path(uploadFile).write_text('wontez morgan') 
---
+++ test.md
@@ -0,0 +1,1 @@
+wontez morgan
