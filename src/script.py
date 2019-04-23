25dd7f06c2ef9a58bcc46dea6234ff506ebe05f6
unknown <instructor@RM56-PC01.commoncore.com>
2019-04-23 12:49:56 -0700

    updates

--- src/script.py
+++ src/script.py
@@ -4,6 +4,16 @@
 from pathlib import Path 
 
 # Creat code to get user input and display user interface
+rosterNumber = input("What is the class number?: ")
+rosterYear = input("What is the class year? ")
+rosterName = rosterNumber + "-" + rosterYear
+createFilename = rosterName + ".txt"
+
+# Template for input
+lineOne = "username,password,firstname,lastname,email,course1,group1,type1"
+lineStudents = rosterNumber + "-stu1,Student1,Student 1,Class " + rosterName + ",1Student" + rosterName + "@cc.mil,Common Core,Class " + rosterName + ",1"
+lineInstructor = rosterNumber +"-inst,###password###,Instructor " + rosterNumber + ",Class " + rosterName + ",Instructor" + rosterName + "@cc.mil,Common Core,Class " + rosterName + ",2"
+allLines = 
 
 
 # Gets home directory and sets working directory to the users Desktop
@@ -11,8 +21,9 @@ path = Path.home()
 desktop = path / 'Desktop'
 
 # Creates file on users Desktop
-uploadFile = desktop / 'newfile.txt'
+uploadFile = desktop / createFilename 
 Path(uploadFile).touch()
 
 # Writes content to the file created on the users Desktop
 Path(uploadFile).write_text('wontez morgan') 
+Path(uploadFile).write_text('will this append') # didn't append  
--- test.md
+++
@@ -1,1 +0,0 @@
-wontez morgan
